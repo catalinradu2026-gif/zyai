425bda8dcb8ad4bbdfa73dd8e2f3f987c3687bfe
Catalin <catalin@cautjob.com>
2026-04-07 05:28:26 +0300

    Fix: import useSearchParams from next/navigation

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useState, useSearchParams } from 'react'
+import { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 
 export default function LoginPage() {
